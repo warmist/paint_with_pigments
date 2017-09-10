bb9c75f4e6f6c25ebcc473385ea0aee7973fb8e9
Warmist <warmist@gmail.com>
2017-09-10 21:29:54 +0300

    almost working

---
+++ assets/shader.glsl
@@ -0,0 +1,147 @@
+//!program main fragment
+#version 330
+
+out vec4 color;
+uniform sampler2DArray color_layers;
+uniform sampler1DArray pigments;
+varying vec3 pos;
+
+uniform float power;
+
+float xFit_1931(  float wave )
+{
+	 float t1 = (wave-442.0f)*((wave<442.0f)?0.0624f:0.0374f);
+	 float t2 = (wave-599.8f)*((wave<599.8f)?0.0264f:0.0323f);
+	 float t3 = (wave-501.1f)*((wave<501.1f)?0.0490f:0.0382f);
+	return 0.362f*exp(-0.5f*t1*t1) + 1.056f*exp(-0.5f*t2*t2)
+	- 0.065f*exp(-0.5f*t3*t3);
+}
+float yFit_1931(  float wave )
+{
+	 float t1 = (wave-568.8f)*((wave<568.8f)?0.0213f:0.0247f);
+	 float t2 = (wave-530.9f)*((wave<530.9f)?0.0613f:0.0322f);
+	return 0.821f*exp(-0.5f*t1*t1) + 0.286f*exp(-0.5f*t2*t2);
+}
+float zFit_1931(float wave )
+{
+	 float t1 = (wave-437.0f)*((wave<437.0f)?0.0845f:0.0278f);
+	 float t2 = (wave-459.0f)*((wave<459.0f)?0.0385f:0.0725f);
+	return 1.217f*exp(-0.5f*t1*t1) + 0.681f*exp(-0.5f*t2*t2);
+}
+vec3 fit_1931(float w)
+{
+	return vec3(xFit_1931(w),yFit_1931(w),zFit_1931(w));
+}
+float black_body_spectrum( float l, float temperature )
+{
+	/*float h=6.626070040e-34; //Planck constant
+	float c=299792458; //Speed of light
+	float k=1.38064852e-23; //Boltzmann constant
+	*/
+	 float const_1=5.955215e-17;//h*c*c
+	 float const_2=0.0143878;//(h*c)/k
+	 float top=(2*const_1);
+	 float bottom=(exp((const_2)/(temperature*l))-1)*l*l*l*l*l;
+	return top/bottom;
+	//return (2*h*freq*freq*freq)/(c*c)*(1/(math.exp((h*freq)/(k*temperature))-1))
+}
+vec3 get_bb_xyz(float t)
+{
+	vec3 lab=vec3(0,0,0);
+	for(float l=380;l<=730;l+=10)
+	{
+		lab+=fit_1931(l)*black_body_spectrum(l*1e-9,t);
+	}
+	return lab;
+}
+vec3 xyz2rgb(vec3 c ) {
+	const mat3 mat = mat3(
+        3.2406, -1.5372, -0.4986,
+        -0.9689, 1.8758, 0.0415,
+        0.0557, -0.2040, 1.0570
+	);
+    vec3 v =c*mat;// mul(c , mat);
+    vec3 r;
+    r.x = ( v.r > 0.0031308 ) ? (( 1.055 * pow( v.r, ( 1.0 / 2.4 ))) - 0.055 ) : 12.92 * v.r;
+    r.y = ( v.g > 0.0031308 ) ? (( 1.055 * pow( v.g, ( 1.0 / 2.4 ))) - 0.055 ) : 12.92 * v.g;
+    r.z = ( v.b > 0.0031308 ) ? (( 1.055 * pow( v.b, ( 1.0 / 2.4 ))) - 0.055 ) : 12.92 * v.b;
+    return r;
+}
+float sample_pigment(float pos,int pig_id)
+{
+	return texelFetch(pigments,ivec2(int((pos-380)/10),pig_id),0).x;
+}
+vec3 get_bb_pigment(float t,int id)
+{
+	vec3 lab=vec3(0,0,0);
+	for(float l=380;l<=730;l+=10)
+	{
+		lab+=fit_1931(l)*black_body_spectrum(l*1e-9,t)*sample_pigment(l,id);
+	}
+	return lab;
+}
+vec3 get_bb_pigment_mix(float t,int id,int id2,float w1,float w2)
+{
+	vec3 lab=vec3(0,0,0);
+	for(float l=380;l<=730;l+=10)
+	{
+		float p1=sample_pigment(l,id);
+		float p2=sample_pigment(l,id2);
+		float mean=pow(pow(p1,w1)*pow(p2,w2),1/(w1+w2));
+		lab+=fit_1931(l)*black_body_spectrum(l*1e-9,t)*mean;
+	}
+	return lab;
+}
+vec3 sample_pixel(vec2 pos,float t)
+{
+	float w[5];
+	for(int i=0;i<5;i++)
+	{
+		w[i]=texture(color_layers, vec3(pos.xy, i)).x;
+	}
+
+	vec3 lab=vec3(0,0,0);
+	for(float l=380;l<=730;l+=10)
+	{
+		float w_sum=0;
+		float x_mean=1;
+		for(int i=0;i<5;i++)
+		{
+			if(w[i]>0.0000001)
+			{
+				x_mean+=log(sample_pigment(l,i))*w[i];
+				w_sum+=w[i];
+			}
+		}
+		if(w_sum>0.00000001)
+		{
+			x_mean=exp(x_mean/w_sum);
+			lab+=fit_1931(l)*black_body_spectrum(l*1e-9,t)*x_mean;
+		}
+	}
+	return lab;
+}
+
+void main(){
+	vec2 pn=(pos.xy+vec2(1))/2;
+	int pig_id=int(pn.y*5);
+
+	float pow_v=pow(10,power*30);
+	//color = vec4(texture(color_layers, vec3(pos.xy, 0)));
+	//color = vec4(texture(pigments, pos.xy*3));
+	vec3 bb=sample_pixel((pos.xy+vec2(1))/2,5800);
+
+	//vec3 bb;
+	/*if(pn.x<0.5)
+		bb=get_bb_pigment(5800,pig_id);
+	else
+		bb=get_bb_pigment_mix(5800,pig_id,2,power,(1-power));*/
+	//if(bb.y>0.00001)
+	{
+		bb/=pow_v;
+	}
+	color=vec4(xyz2rgb(bb),1);
+	/*
+	float v=texelFetch(pigments,ivec2(pn.x*40,pig_id),0).x;
+	color=vec4(v,v,v,1);*/
+}
--- src/main.cpp
+++ src/main.cpp
@@ -12,17 +12,9 @@ static void error_callback(int error, const char* description)
 {
     fprintf(stderr, "Error %d: %s\n", error, description);
 }
-
-void make_gui()
-{
-    ImGuiIO& io = ImGui::GetIO();
-    ImGui::Begin("Window");
-    ImGui::Text("Hello world");
-    ImGui::End();
-}
-const int max_nm = 720;
+const int max_nm = 730;
 const int min_nm = 380;
-const int count_color_samples = (max_nm - min_nm) / 10;
+const int count_color_samples = (max_nm - min_nm) / 10+1;
 void load_color_samples(GLuint texture,int count_colors)
 {
 	std::ifstream f("colors.txt");
@@ -30,12 +22,17 @@ void load_color_samples(GLuint texture,int count_colors)
 	std::vector<float> buffer(count_color_samples);
 	for(int j=0;j<count_colors;j++)
 	{
+		std::cout << "COLOR:" << j << std::endl;
 		for (int i = 0; i < count_color_samples; i++)
 		{
-			f >> buffer[i];
-			if (buffer[i] > 1)buffer[i] = 1;
-			if (buffer[i] < 0)buffer[i] = 0;
+			float v;
+			f >> v;
+			if (v > 1)v = 1;
+			if (v < 0)v = 0;
+			buffer[i] = v;
+			std::cout << v << " ";
 		}
+		std::cout << std::endl;
 		glTexSubImage2D(GL_TEXTURE_1D_ARRAY, 0,
 			0, j, 
 			count_color_samples, 1,
@@ -51,7 +48,7 @@ struct pixel_buffer
 
 	int index = 0;
 	int next_index = 1;
-	const int count_colors = 3;
+	const int count_colors = 5;
 	std::vector<float> buffer;
 	int w, h;
 
@@ -67,7 +64,7 @@ struct pixel_buffer
 		glTexParameteri(GL_TEXTURE_1D_ARRAY, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
 		glTexParameteri(GL_TEXTURE_1D_ARRAY, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 
-		//380-720 every 10 nm
+		//380-730 every 10 nm
 		
 		glTexImage2D(GL_TEXTURE_1D_ARRAY, 0, GL_R32F, count_color_samples, count_colors, 0, GL_RED, GL_FLOAT, NULL);
 		load_color_samples(texture_colors,count_colors);
@@ -81,56 +78,45 @@ struct pixel_buffer
 		if (screen_size == w*h)
 			return;
 		screen_size = GLsizeiptr(w*h);
-		const unsigned pixel_size = sizeof(float);
 
-		buffer.resize(screen_size*pixel_size);
+		buffer.resize(screen_size*count_colors);
 
 		glBindTexture(GL_TEXTURE_2D_ARRAY, texture);
 		glTexParameteri(GL_TEXTURE_2D_ARRAY, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
 		glTexParameteri(GL_TEXTURE_2D_ARRAY, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-		//glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAX_LEVEL, 0);
 		glBindBuffer(GL_PIXEL_UNPACK_BUFFER, 0);
-		//glTexStorage2D(GL_TEXTURE_2D, 1, GL_RGBA16F, (int)io.DisplaySize.x, (int)io.DisplaySize.y );
 		glTexImage3D(GL_TEXTURE_2D_ARRAY, 0, GL_R32F, w, h, count_colors, 0, GL_RED, GL_FLOAT, NULL);
-
-		//GL_INVALID_ENUM
-		// Because we're also using this tex as an image (in order to write to it),
-		// we bind it to an image unit as well
-		//glBindImageTexture(0, texture, 0, GL_FALSE, 0, GL_WRITE_ONLY, GL_R32F);
-
-		/*
-		glBindFramebuffer(GL_FRAMEBUFFER, framebuffer);
-
-		glBindTexture(GL_TEXTURE_2D, framebuffer_tex);
-		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA32F, (int)io.DisplaySize.x, (int)io.DisplaySize.y, 0, GL_RGBA, GL_FLOAT, NULL);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-
-		glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, framebuffer_tex, 0);
-		glBindFramebuffer(GL_FRAMEBUFFER, 0);
-		*/
+	}
+	void set_pixel(int x, int y, int d,float v)
+	{
+		buffer[x + (h-y)*w+d*w*h] = v;
+	}
+	float& pixel(int x, int y, int d)
+	{
+		return buffer[x + (h - y)*w + d*w*h];
 	}
 	void push_data()
 	{
 		glBindTexture(GL_TEXTURE_2D_ARRAY, texture);
-		glTexSubImage3D(GL_TEXTURE_2D_ARRAY, 0,
-			0, 0, 0,
-			w, h, count_colors,
-			GL_RED, GL_FLOAT,
-			buffer.data());
+		//for(int i=0;i<count_colors;i++)
+		//{
+			glTexSubImage3D(GL_TEXTURE_2D_ARRAY, 0,
+				0, 0, 0,
+				w, h, count_colors,
+				GL_RED, GL_FLOAT,
+				buffer.data());
+		//}
 		
 	}
 };
 void print_prog_status(GLuint id,GLint stat,const std::vector<char>& log)
 {
 	if (stat == GL_TRUE)
-		std::cout << id << " is ok!";
+		std::cout << id << " is ok!"<<std::endl;
 	else
 	{
 		if (log.size() > 0)
-			std::cout << "Log:" << log.data();
+			std::cout << "Log:" << log.data() << std::endl;
 	}
 }
 void get_shader_log(GLuint id)
@@ -158,7 +144,7 @@ GLuint make_shader(std::string text, GLuint type)
 	get_shader_log(ret);
 	return ret;
 }
-GLuint init_shader(GLuint& tex_uniform,GLuint& pigments_uniform)
+GLuint init_shader(GLuint& tex_uniform,GLuint& pigments_uniform,GLuint& power_uniform)
 {
 	GLuint prog_id = glCreateProgram();
 	
@@ -178,58 +164,10 @@ GLuint init_shader(GLuint& tex_uniform,GLuint& pigments_uniform)
 	}
 	)", GL_VERTEX_SHADER);
 
-	GLuint s_frag = make_shader(R"(
-	//!program main fragment
-	#version 330
-
-
-float xFit_1931(  float wave )
-{
-	 float t1 = (wave-442.0f)*((wave<442.0f)?0.0624f:0.0374f);
-	 float t2 = (wave-599.8f)*((wave<599.8f)?0.0264f:0.0323f);
-	 float t3 = (wave-501.1f)*((wave<501.1f)?0.0490f:0.0382f);
-	return 0.362f*exp(-0.5f*t1*t1) + 1.056f*exp(-0.5f*t2*t2)
-	- 0.065f*exp(-0.5f*t3*t3);
-}
-float yFit_1931(  float wave )
-{
-	 float t1 = (wave-568.8f)*((wave<568.8f)?0.0213f:0.0247f);
-	 float t2 = (wave-530.9f)*((wave<530.9f)?0.0613f:0.0322f);
-	return 0.821f*exp(-0.5f*t1*t1) + 0.286f*exp(-0.5f*t2*t2);
-}
-float zFit_1931(  float wave )
-{
-	 float t1 = (wave-437.0f)*((wave<437.0f)?0.0845f:0.0278f);
-	 float t2 = (wave-459.0f)*((wave<459.0f)?0.0385f:0.0725f);
-	return 1.217f*exp(-0.5f*t1*t1) + 0.681f*exp(-0.5f*t2*t2);
-}
-vec3 fit_1931( float w)
-{
-	return vec3(xFit_1931(w),yFit_1931(w),zFit_1931(w));
-}
-float black_body_spectrum( float l, float temperature )
-{
-	/*float h=6.626070040e-34; //Planck constant
-	float c=299792458; //Speed of light
-	float k=1.38064852e-23; //Boltzmann constant
-	*/
-	 float const_1=5.955215e-17;//h*c*c
-	 float const_2=0.0143878;//(h*c)/k
-	 float top=(2*const_1);
-	 float bottom=(exp((const_2)/(temperature*l))-1)*l*l*l*l*l;
-	return top/bottom;
-	//return (2*h*freq*freq*freq)/(c*c)*(1/(math.exp((h*freq)/(k*temperature))-1))
-}
-
-	out vec4 color;
-	uniform sampler2DArray color_layers;
-	uniform sampler1DArray pigments;
-	varying vec3 pos;
-	void main(){
-		//color = vec4(texture(color_layers, vec3(pos.xy, 0)));
-		color = vec4(texture(pigments, pos.xy*3));
-	}
-	)", GL_FRAGMENT_SHADER);
+	std::ifstream shade_f("shader.glsl");
+	std::string shade_text;
+	std::getline(shade_f, shade_text, '\0');
+	GLuint s_frag = make_shader(shade_text, GL_FRAGMENT_SHADER);
 	// Check Shader
 	//glGetShaderiv(s_id, GL_COMPILE_STATUS, &tmp_s.status.result);
 	//get_shader_log(tmp_s, ctx);
@@ -240,6 +178,7 @@ float black_body_spectrum( float l, float temperature )
 
 	tex_uniform = glGetUniformLocation(prog_id, "color_layers");
 	pigments_uniform = glGetUniformLocation(prog_id, "pigments");
+	power_uniform = glGetUniformLocation(prog_id, "power");
 
 	return prog_id;
 }
@@ -266,10 +205,10 @@ int main(int, char**)
     glfwMakeContextCurrent(window);
     
     gl3wInit();
-
+#ifdef DEBUG_GL
 	glDebugMessageCallback(&dgb_callback, nullptr);
 	glEnable(GL_DEBUG_OUTPUT);
-
+#endif
     // Setup ImGui binding
     ImGui_ImplGlfwGL3_Init(window, true);
 
@@ -294,23 +233,58 @@ int main(int, char**)
 	glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
 	GLuint texture_loc;
 	GLuint pigments_loc;
+	GLuint power_loc;
+	auto prog = init_shader(texture_loc, pigments_loc,power_loc);
 	
-	auto prog = init_shader(texture_loc, pigments_loc);
-	
+	float power = 0.529;
+	int color_id = 0;
+	const int rad = 10;
     // Main loop
-    while (!glfwWindowShouldClose(window))
-    {
-        
-        ImGuiIO& io = ImGui::GetIO();
-       
-        glfwPollEvents();
-       
-        ImGui_ImplGlfwGL3_NewFrame();
+	while (!glfwWindowShouldClose(window))
+	{
 
-        make_gui();
+		ImGuiIO& io = ImGui::GetIO();
+
+		glfwPollEvents();
+
+		ImGui_ImplGlfwGL3_NewFrame();
+
+		ImGui::Begin("Settings");
+		ImGui::SliderFloat("Power", &power, 0, 1);
+		ImGui::SliderInt("Color", &color_id, 0, pbuf.count_colors-1);
+		ImGui::End();
 		pbuf.update_buffer_size(io);
-		for (int i = 0; i < pbuf.buffer.size(); i++)
-			pbuf.buffer[i] = 0.5;
+
+		if (io.MouseWheel)
+		{
+			color_id += io.MouseWheel;
+			if (color_id >= pbuf.count_colors)
+				color_id = 0;
+			if (color_id < 0)
+				color_id = pbuf.count_colors - 1;
+		}
+		if( (io.MouseDown[0] && !io.MouseDownOwned[0]) || (io.MouseDown[1] && !io.MouseDownOwned[1]))
+		{
+			float dir = 1;
+			if (io.MouseDown[1])
+				dir = -1;
+
+			for(int y=-rad;y<rad;y++)
+			for (int x = -rad; x < rad; x++)
+			{
+				if(x*x+y*y<rad*rad)
+				{
+					int tx = io.MousePos.x + x;
+					int ty = io.MousePos.y + y;
+					if (tx >= 0 && ty >= 0 && tx < pbuf.w && ty < pbuf.h)
+					{
+						auto& f = pbuf.pixel(tx, ty, color_id);
+						f += 0.1*dir;
+						if (f < 0)f = 0;
+					}
+				}
+			}
+		}
 		pbuf.push_data();
         // Rendering
 		glViewport(0, 0, (int)io.DisplaySize.x, (int)io.DisplaySize.y);
@@ -335,8 +309,11 @@ int main(int, char**)
 		glActiveTexture(GL_TEXTURE0 + 1);
 		glBindTexture(GL_TEXTURE_1D_ARRAY, pbuf.texture_colors);
 		glUniform1i(pigments_loc, 1);
+		glUniform1f(power_loc, power);
+
 		glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
 
+		
 		glDisableVertexAttribArray(0);
 		glUseProgram(0);
         ImGui::Render();
